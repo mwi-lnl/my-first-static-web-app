f7c85f92ae9a37fcd59b8b857c0a46c087236fdd
maurizio_wille <maurizio.wille@loterie.lu>
2025-07-22 15:55:01 +0200

    First Commit

--- BlazorAppStandalone/Program.cs
+++ BlazorAppStandalone/Program.cs
@@ -1,6 +1,9 @@
-using BlazorBasic;
+using BlazorAppStandalone;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+// npm cache clean --force
+// https://dvoituron.com/2024/05/03/azure-deploy-static/
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
